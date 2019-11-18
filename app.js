8e8f070479dc15f2fe198b41c3f31d4dff6eb659
sonihidayatulloh797@gmail.com <sonihidayatulloh797@gmail.com>
2019-11-18 15:17:14 +0700

    update

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 const express      = require('express')
 const app          = express()
 require("./servers/db")
-
-const barangg      = require("./routes/barang.routes")  
-const index_routes = require("./routes/index")
-const karyawan     = require("./routes/pegawai.routes")
+ 
+const index_routes   = require("./routes/index") 
+const books          = require("./routes/buku.routes")  
 
 app.use(express.urlencoded({ extended : true }))
 
-app.use("/barang", barangg)
 app.use("/index", index_routes)
-app.use("/pegawai", karyawan)
+app.use("/buku", books)
 
 app.listen(3300, () => {
-    console.log(`Example in port 3300`)
+    console.log(`Example port to 3300`)
 })
