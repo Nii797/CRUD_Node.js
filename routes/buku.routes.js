5ff85406eeaaba31935b4e664e657ad1d0e499f5
sonihidayatulloh797@gmail.com <sonihidayatulloh797@gmail.com>
2019-11-18 15:15:41 +0700

    update

---
+++ routes/buku.routes.js
@@ -0,0 +1,22 @@
+const express   = require("express")
+const router    = express.Router()
+const bukumodel = ("../models/buku.model")
+
+router.post("/create", async (req, res) => {
+    let { id_buku, judul, penulis, thn_terbit, penerbit } = req.body
+    let input_data = {
+        id_buku, judul, penulis, thn_terbit, penerbit
+    }
+
+    let data = new bukumodel(input_data)
+    data.save()
+
+    return res.send({
+        status: "Success",
+        data,
+        message: "Data buku berhasil dimasukan"
+    })    
+})
+
+
+module.exports = router
